77fd17acdda9ec94a37c3f7507adb89202f401af
Utkarsh Palav <u.palav@student.tudelft.nl>
2025-11-25 17:57:21 +0100

    similarity score Q1 part 2 solved

--- legacy_2024/2024_c1.cpp
+++ legacy_2024/2024_c1.cpp
@@ -4,71 +4,25 @@
 #include <vector>
 #include <algorithm>
 #include "helpers/line_parser.hpp"
+#include <set>
 
 using namespace std;
 
 string eachLine;
-// int i = 0;
 long sum = 0;
+long similarity_score = 0;
 
-// struct {
-//     int item_L;
-//     int item_R;
-// } values;
 
 vector <int> leftList;
 vector <int> rightList;
 vector <int> distace;
 
 
-// void extractValues(string lineInput){
-
-//     lineInput.push_back(' ');
-//     string tmpArr;
-//     int i = 0;
-//     int catcher = 0;
-
-//     while (i < (lineInput.length() + 1)){
-//         if (lineInput[i] != ' '){
-//             tmpArr += lineInput[i];
-//         }
-//         else{
-//             if (tmpArr.length() > 2){
-                
-//                 switch (catcher)
-//                 {
-//                 case 0:
-//                     // cout << "Trying to convert value : " << tmpArr << '\n';
-//                     values.item_L = stoi(tmpArr);
-//                     catcher ++;
-//                     tmpArr.erase();
-//                     break;
-
-//                 case 1:
-//                     values.item_R = stoi(tmpArr);
-//                     break;
-
-//                 default:
-//                     cout << "unknown case occured" << '\n';
-//                     break;
-//                 }
-//            }
-//         }
-//         i++;
-//     }
-
-//     // cout << "Extracted Values : " << values.item_L << " " << values.item_R << '\n';
-
-// }
-
 int main(){
     ifstream readTxtFile("data_dump.txt");
     cout << "Tried to read file" << '\n';
     if (readTxtFile.is_open()){
         while (getline(readTxtFile, eachLine)){
-            // extractValues(eachLine);
-            // leftList.push_back(values.item_L);
-            // rightList.push_back(values.item_R);
             auto [left, right] = parseLineToTwoInts(eachLine);
             leftList.push_back(left);
             rightList.push_back(right);
@@ -77,6 +31,7 @@ int main(){
     else{
         cout << "Could not open requested file" << '\n';
     }
+    readTxtFile.close();
 
     sort(leftList.begin(), leftList.end());
     sort(rightList.begin(), rightList.end());
@@ -86,14 +41,38 @@ int main(){
 
     for (int j=0; j<leftList.size(); j++){
         int val = abs(leftList.at(j) - rightList.at(j));
-        // cout << val << '\n';
         distace.push_back(val);
         sum += val;
     }
 
     cout << "Final value: " << sum << '\n';
 
-    readTxtFile.close();
+    // similarity score
+
+    set <int> uniqueLeftSet (leftList.begin(), leftList.end());
+    unordered_map <int, int> occurenceMap;
+
+    cout << "Number of unique elements in leftList: " << uniqueLeftSet.size() << '\n';
+
+    for (auto it = uniqueLeftSet.begin(); it != uniqueLeftSet.end(); ++it){
+        occurenceMap.insert(occurenceMap.begin(), pair<int, int>(*it, 0));
+    }
+
+    for (auto i : rightList){
+        auto it = occurenceMap.find(i);
+        if (it != occurenceMap.end()){
+            int tmp = it->second;
+            occurenceMap.at(it->first) = tmp + 1;
+        }
+    }
+
+    for (auto x : occurenceMap){
+        similarity_score += (x.first * x.second);
+    }
+
+    cout << "Similarity Score : " << similarity_score << endl;
+    
+    
 }
 
 
